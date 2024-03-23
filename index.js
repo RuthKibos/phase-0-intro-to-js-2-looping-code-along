4e7437cffd19df7c5eb901630149cc0a8ca0c8d9
RuthKibos <kibosruth@gmail.com>
2024-03-23 23:46:39 +0300

    Completed the Lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+  const thankYouMessages = [];
+  for (let i = 0; i < names.length; i++) {
+    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return thankYouMessages;
+}
+function countDown(num) {
+  if (num <= 0 || !Number.isInteger(num)) {
+    console.log("Please provide a positive integer.");
+    return;
+  }
+while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
+countDown(10)
